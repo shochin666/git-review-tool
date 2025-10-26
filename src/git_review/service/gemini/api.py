52c04fb58bb62eb2ee35e175787fda2ac69e116f
pastellcolour <hayabsos@gmail.com>
2025-10-26 13:39:24 +0900

    レンダリングができていなかった不具合を修正

--- src/git_review/service/gemini/api.py
+++ src/git_review/service/gemini/api.py
@@ -27,11 +27,14 @@ def get_gemini_response(prompt: str) -> str:
 if __name__ == "__main__":
     env = Environment(loader=FileSystemLoader("src/git_review/gemini/templates"))
     query_template = env.get_template("generate_commitmessage.jinja")
-    
+
     query = query_template.render(task_title="ユーザー登録時のメールアドレス重複チェック機能を追加",
                                   task_description="ユーザーの誤登録防止のため、新規登録時に入力されたメールアドレスが既にデータベースに存在するかを確認する機能が必要です。",
                                   git_diff="`+ if User.objects.filter(email=email).exists():`",
                                   impact_scope="est1.py line45 test2.py line78"
                                   )
     result = get_gemini_response(prompt=query)
+
+    print("*" * 30)
     print(result)
+    print("*" * 30)
